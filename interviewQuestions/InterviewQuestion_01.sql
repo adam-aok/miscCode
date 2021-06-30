16d26afeff2ad6a40e1c37e0e0462c594a39bf08
Adam Keefe <adamkeefe.aok@gmail.com>
2021-06-30 11:15:56 -0400

    fixed syntax

--- interviewQuestions/InterviewQuestion_01.sql
+++ interviewQuestions/InterviewQuestion_01.sql
@@ -1,5 +1,6 @@
 /* Simple interview question to pull a list of account IDs from a database which are duplicated in the table. 9-23-2020 */
 SELECT id, COUNT(id)
 FROM db_accounts
-HAVING COUNT(id) > 1
 GROUP by id
+HAVING COUNT(id) > 1
+
